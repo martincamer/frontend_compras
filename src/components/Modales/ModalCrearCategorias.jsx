09b765effd7b9abf38c693ca56a4419a33276239
martincamer <martincamer011@gmail.com>
2024-03-26 15:50:37 -0300

    SAD

--- src/components/Modales/ModalCrearCategorias.jsx
+++ src/components/Modales/ModalCrearCategorias.jsx
@@ -109,7 +109,7 @@ export const ModalCrearCategorias = ({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-1/3 max-md:w-full p-6 my-8 overflow-hidden max-md:h-[300px] max-md:overflow-y-scroll text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <div className="inline-block w-1/2 max-md:w-full p-6 my-8 overflow-hidden max-md:h-[300px] max-md:overflow-y-scroll text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                 <div className="text-sm text-slate-700 mb-3 border-b-[1px] uppercase">
                   Crear nueva categoria
                 </div>
@@ -141,15 +141,44 @@ export const ModalCrearCategorias = ({
                       Categorias creadas
                     </p>
                   </div>
-                  <div className="py-2 px-0 flex gap-3">
+                  <div className="py-2 px-0 grid grid-cols-3 gap-3">
                     {categorias.map((c) => (
                       <div
-                        className="border-slate-300 border-[1px] py-1 px-3 rounded-xl"
+                        className="border-slate-300 border-[1px] py-1 px-3 rounded-xl flex gap-2 items-center"
                         key={c.id}
                       >
                         <p className="uppercase font-bold text-sm text-slate-600">
                           {c.detalle}
                         </p>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-5 h-5 text-blue-500"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                          />
+                        </svg>
+
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-5 h-5 text-red-500"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                          />
+                        </svg>
                       </div>
                     ))}
                   </div>
